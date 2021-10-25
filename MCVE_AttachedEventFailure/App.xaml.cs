5d901474832fcf55f59549050323740e725140eb
William Vincent <AbbottWC@Gmail.com>
2021-10-25 15:47:14 -0500

    Add project files.

---
+++ MCVE_AttachedEventFailure/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MCVE_AttachedEventFailure {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+		//Field Value
+		private RelayCommand testCommand = new RelayCommand(( ) => MessageBox.Show("Event Captured!", "Success!"));
+		public RelayCommand TestCommand => testCommand;
+	}
+}
